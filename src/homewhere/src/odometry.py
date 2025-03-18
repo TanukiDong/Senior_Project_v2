19a1f95972d7a24e07b8beafd1dfd3ecd626c17a
TanukiDong <europazas@hotmail.com>
2025-03-18 16:51:07 +0700

    test odom

--- src/homewhere/src/odometry.py
+++ src/homewhere/src/odometry.py
@@ -7,53 +7,24 @@ import tf
 import math
 from sensor_msgs.msg import JointState
 
-# Wheel base distance
 WHEEL_RADIUS = 0.07
-# WHEEL_BASE = 0.5 / 2
 
-# Initialize global variables for wheel velocities
-velFrontLeft_Linear = 0.0
-velFrontLeft_Angular = 0.0
-velFrontRight_Linear = 0.0
-velFrontRight_Angular = 0.0
-velBackLeft_Linear = 0.0
-velBackLeft_Angular = 0.0
-velBackRight_Linear = 0.0
-velBackRight_Angular = 0.0
+vx_cmd = 0.0
+vy_cmd = 0.0
 
-# Callback functions to update wheel velocities
-def front_left_callback(msg):
-    global velFrontLeft_Linear, velFrontLeft_Angular
-    velFrontLeft_Linear = msg.linear.x
-    velFrontLeft_Angular = msg.angular.z
-
-def front_right_callback(msg):
-    global velFrontRight_Linear, velFrontRight_Angular
-    velFrontRight_Linear = msg.linear.x
-    velFrontRight_Angular = msg.angular.z
-
-def back_left_callback(msg):
-    global velBackLeft_Linear, velBackLeft_Angular
-    velBackLeft_Linear = msg.linear.x
-    velBackLeft_Angular = msg.angular.z
-
-def back_right_callback(msg):
-    global velBackRight_Linear, velBackRight_Angular
-    velBackRight_Linear = msg.linear.x
-    velBackRight_Angular = msg.angular.z
+def cmd_vel_callback(msg):
+    global vx_cmd, vy_cmd
+    vx_cmd = msg.linear.x
+    vy_cmd = msg.linear.y
 
 def odometry_publisher():
     rospy.init_node('odometry')
     odom_pub = rospy.Publisher('/odom', Odometry, queue_size=50)
     odom_broadcaster = tf.TransformBroadcaster()
-
-    # Subscribe to each wheel's velocity topic
-    rospy.Subscriber("/cmd_vel_front_left", Twist, front_left_callback)
-    rospy.Subscriber("/cmd_vel_front_right", Twist, front_right_callback)
-    rospy.Subscriber("/cmd_vel_back_left", Twist, back_left_callback)
-    rospy.Subscriber("/cmd_vel_back_right", Twist, back_right_callback)
     
     joint_pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
+    
+    rospy.Subscriber('/cmd_vel', Twist, cmd_vel_callback)
 
     # Initial position and orientation
     x = y = theta = 0.0
@@ -65,26 +36,18 @@ def odometry_publisher():
         dt = (current_time - last_time).to_sec()
         last_time = current_time
 
-        # Calculation
-        v = velFrontLeft_Linear * WHEEL_RADIUS
-        vx = v * math.cos(theta)
-        vy = v * math.sin(theta)
-        omega = velFrontLeft_Angular
+        vx = vx_cmd
+        vy = vy_cmd
 
-        dx = vx * dt
-        dy = vy * dt
-        dtheta = omega * dt
-        
-        x += dx
-        y += dy
-        theta += dtheta
+        x += vx * dt
+        y += vy * dt
 
         # Create a quaternion from theta
         odom_quat = tf.transformations.quaternion_from_euler(0, 0, theta)
 
         odom_broadcaster.sendTransform(
             (x, y, 0.0),
-            tf.transformations.quaternion_from_euler(0, 0, theta),
+            odom_quat,
             current_time,
             "base_footprint",
             "odom"
@@ -110,6 +73,7 @@ def odometry_publisher():
         odom = Odometry()
         odom.header.stamp = current_time
         odom.header.frame_id = "odom"
+        odom.child_frame_id = "base_footprint"
 
         # Set the position
         odom.pose.pose.position.x = x
@@ -118,15 +82,14 @@ def odometry_publisher():
         odom.pose.pose.orientation = Quaternion(*odom_quat)
 
         # Set the velocity
-        odom.child_frame_id = "base_footprint"
-        odom.twist.twist.linear.x = v
-        odom.twist.twist.linear.y = 0.0
-        odom.twist.twist.angular.z = omega
+        
+        odom.twist.twist.linear.x = vx
+        odom.twist.twist.linear.y = vy
 
         # Publish the odometry message
         odom_pub.publish(odom)
         
-        
+
         # Create a JointState message
         joint_state = JointState()
         joint_state.header.stamp = current_time
