4594336d3a6ca3265800fea312682f517d44b6ba
GemsLoveNLP <6438169421@student.chula.ac.th>
2025-03-27 23:02:38 +0700

    add: Ackermann control

--- src/homewhere/src/control.py
+++ src/homewhere/src/control.py
@@ -9,6 +9,10 @@ from std_msgs.msg import Int16, Float32
 import signal
 import sys
 
+# Constants
+H = 0.2
+W = 0.2
+
 class Control:
     def __init__(self):
         rospy.init_node('control')
@@ -32,7 +36,10 @@ class Control:
         self.front_right_velocity = Twist()
         self.back_left_velocity = Twist()
         self.back_right_velocity = Twist()
-        self.angle = 90
+        self.angle_fl = 90
+        self.angle_fr = 90
+        self.angle_bl = 90
+        self.angle_br = 90
         self.manual_velocity = 0.1
 
         # Set up keyboard listener
@@ -59,9 +66,37 @@ class Control:
         return int(rad/math.pi*180)
 
     def get_v_and_theta(self, vx, vy):
+        """Calculate the swerve speed and angle
+                vx: the speed of the CG in the x-direction in m/s
+                vy: the speed of the CG in the y-direction in m/s
+            return: tuple of (v,theta)"""
         v = (vx**2 + vy**2)**0.5
         theta = math.atan2(vy, vx)
         return v, theta
+    
+    def get_delta_v_and_theta(self, v, w):
+        """Calculate the Ackermann angle and speed difference
+                v: the speed of the CG in m/s
+                w: the angular velocity in the +z direction in rad/s
+            return: tuple of four delta velocities and thetas ((4),(4))"""
+        r = 0
+        if v == 0 or w == 0:
+            return ((0,0,0,0),(0,0,0,0))
+        else:
+            r = v/w
+
+        hor_fl = r-W if r > 0 else W-r
+        hor_fr = r+W if r > 0 else W+r
+
+        dtheta_fl = math.atan(H, hor_fl)
+        dtheta_fr = math.atan(H, hor_fr)
+        dtheta_bl = -dtheta_fl
+        dtheta_br = -dtheta_fr
+
+        dv_l = w*(H**2 + hor_fl)**0.5
+        dv_r = w*(H**2 + hor_fr)**0.5
+
+        return ((dv_l,dv_r,dv_l,dv_r),(dtheta_fl,dtheta_fr,dtheta_bl,dtheta_br))
 
     def real_velocity(self, v, theta):
         if -math.pi/2 <= theta and theta <= math.pi/2:
@@ -81,21 +116,21 @@ class Control:
         # Extract linear and angular velocities from the cmd_vel message
         vx = msg.linear.x
         vy = msg.linear.y
-
-        print("vx, vy",vx,vy)
+        w = msg.angular.z
         
         v, theta = self.get_v_and_theta(vx, vy)
+        dv_list, dtheta_list = self.get_delta_v_and_theta(v,w)
+        v_list = [v + dv for dv in dv_list]
+        theta_list = [theta + dtheta for dtheta in dtheta_list]
 
-        print("v, theta",v,theta)
-
-        real_v = self.real_velocity(v,theta)
-        real_t = self.real_theta(theta)
-        real_t = self.rad2deg(theta)
+        real_v = [self.real_velocity(v_i,theta_i) for v_i,theta_i in zip(v_list,theta_list)]
+        real_t = [self.real_theta(theta_i) for theta_i in theta_list]
+        real_t = [self.rad2deg(theta_i) for theta_i in real_t]
 
         print("Real",real_v,real_t)
 
         # Apply the computed velocities
-        self.set_velocity([[real_v]*4, real_t])
+        self.set_velocity([real_v, real_t])
 
         # Publish the updated velocities to each wheel
         self.publish_velocity()
@@ -107,7 +142,10 @@ class Control:
         self.back_left_velocity_publisher.publish(self.back_left_velocity)
         self.back_right_velocity_publisher.publish(self.back_right_velocity)
 
-        self.theta_publisher.publish(int(self.angle))
+        self.theta_fl_publisher.publish(int(self.angle_fl))
+        self.theta_fr_publisher.publish(int(self.angle_fr))
+        self.theta_bl_publisher.publish(int(self.angle_bl))
+        self.theta_br_publisher.publish(int(self.angle_br))
 
     def set_velocity(self, vel):
         """Set velocity for each individual wheel."""
@@ -117,7 +155,10 @@ class Control:
         self.back_left_velocity.linear.x = vel[0][2]
         self.back_right_velocity.linear.x = vel[0][3]
 
-        self.angle = vel[1]
+        self.angle_fl = vel[1][0]
+        self.angle_fr = vel[1][1]
+        self.angle_bl = vel[1][2]
+        self.angle_br = vel[1][3]
             
     def on_press(self, key):
         """Handle key press events."""
@@ -133,7 +174,7 @@ class Control:
 
             # Compute velocity and angular velocity based on active keys
             velocity = still
-            angle = 0
+            angle = [0]*4
 
             if 'w' in self.active_keys:  # Forward
                 velocity = forward
@@ -141,10 +182,10 @@ class Control:
                 velocity = backward
             if 'a' in self.active_keys:  # Left turn
                 velocity = forward
-                angle = 90
+                angle = [90]*4
             if 'd' in self.active_keys:  # Right turn
                 velocity = backward
-                angle = 90
+                angle = [90]*4
 
             self.set_velocity([velocity, angle])
             self.publish_velocity()
@@ -156,7 +197,7 @@ class Control:
         """Handle key release events."""
         self.manual_control_active = False
         self.active_keys.discard(key.char)
-        self.set_velocity([[0.0, 0.0, 0.0, 0.0],0])
+        self.set_velocity([[0.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0]])
         self.publish_velocity()
 
     def run(self):
