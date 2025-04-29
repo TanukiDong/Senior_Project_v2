4ad61c73c6ae776bf24a57d896db3700c904c6a0
GemsLoveNLP <6438169421@student.chula.ac.th>
2025-04-29 10:27:53 +0700

    add: new servo

--- src/homewhere/src/hardware/arduino_control.py
+++ src/homewhere/src/hardware/arduino_control.py
@@ -32,6 +32,27 @@ class Arduino:
         time.sleep(2)  # Allow time for Arduino to initialize
         self.ser = ser
 
+        # wheels
+        y = [(12,76,150),
+            (22,92,163),
+            (13,86,150),
+            (24,94,160)]
+        self.coefs = [self.calculate_piecewise_linear_params(y_points=tup) for tup in y]
+
+    def calculate_piecewise_linear_params(self,y_points, x_points=[0,90,180]):
+        if len(x_points) != 3 or len(y_points) != 3:
+            raise ValueError("Exactly three points are required.")
+        
+        # First segment (low x to mid x)
+        slope1 = (y_points[1] - y_points[0]) / (x_points[1] - x_points[0])
+        intercept1 = y_points[0] - slope1 * x_points[0]
+        
+        # Second segment (mid x to high x)
+        slope2 = (y_points[2] - y_points[1]) / (x_points[2] - x_points[1])
+        intercept2 = y_points[1] - slope2 * x_points[1]
+        
+        return (slope1, intercept1), (slope2, intercept2)
+
     def write(self,string):
         """Encode and Write the string to the Arduino"""
         self.ser.write(string.encode())
@@ -87,12 +108,13 @@ class Arduino:
         # Servo 8: Bottom Left -> 95 degrees
         # Servo 9: Top Left -> 98 degrees
         # angle += 90 (to make 0 degrees the center)
-        center = {6:87, 7:90, 8:90, 9:82} 
-        # correct orientation
-        angle = 90 - angle
+        new_angles = [coef1[0]*angle + coef1[1] 
+                      if angle < 90 
+                      else coef2[0]*angle + coef2[1] 
+                      for coef1, coef2 in self.coefs]
         for i in range(6, 10):
             # Calculate new local angle
-            local_angle = angle - 90 + center[i]
+            local_angle = new_angles[i-6]
             if local_angle < 0:
                 local_angle = 0
             elif local_angle > 180:
